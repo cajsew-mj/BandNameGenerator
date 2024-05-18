b17816d7611b4ef586295a723a2129c870249502
Batuhan <batuhansamilwhatsapp@gmail.com>
2024-05-18 15:34:48 +0300

    BandNameGenerator
    A  python code that helps you choose a name

---
+++ Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print(" Welcome to the band generator ! ")
+name_city = input(" What is name of the city you grew up in ? ")
+name_pet = input(" What is your pet's name ? ")
+print(f" Your band name should be {name_city.capitalize()} {name_pet.capitalize()}.")
